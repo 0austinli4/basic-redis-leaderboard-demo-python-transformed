e79061bfa5ae8f2df4d3d96cce0c49dba8277e90
Austin <0austinli4@gmail.com>
2025-02-28 19:30:49 +0800

    dummy client

---
+++ pythonclient.py
@@ -0,0 +1,37 @@
+import json
+from mdlin import SyncAppRequest, InitCustom
+
+
+def init():
+    symbol = "AAPL"
+    SyncAppRequest(
+        "ZADD",
+        "leaderboard",
+        {symbol: 12311400},
+    )
+    SyncAppRequest("HSET", symbol, "company", "APPLE")
+    SyncAppRequest("HSET", symbol, "country", "USA")
+    print("init completed")
+
+
+# test the methods from mdlin
+def get_zrange(start_index, stop_index, desc=True):
+    print("Sending zrange request")
+    companies = SyncAppRequest("ZRANGE", "leaderboard", start_index, stop_index)
+    print("Result, companies", companies)
+
+    return get_result(companies, start_index, desc)
+
+
+def get_result(companies, start_index=0, desc=True):
+    symbol = "AAPL"
+    company_info = SyncAppRequest("HGETALL", symbol)
+    print(company_info)
+
+
+if __name__ == "__main__":
+    InitCustom("0", "multi_paxos")
+    print("Running the python client")
+    init()
+    print("Compelted init")
+    get_zrange(1, 10)
